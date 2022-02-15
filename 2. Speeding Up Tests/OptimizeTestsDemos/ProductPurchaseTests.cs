477f6b7710320d72610864f10cb43d8e6f551ec4
Anton Angelov <aangelov@automatetheplanet.com>
2022-02-15 15:11:30 +0200

    add chapter 2 exercises

---
+++ 2. Speeding Up Tests/OptimizeTestsDemos/ProductPurchaseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace StabilizeTestsDemos.SixthVersion
+{
+    [TestClass]
+    public class ProductPurchaseTests : BaseTest
+    {   
+
+        private string GetUserPasswordFromDb(string userName)
+        {
+            return "@purISQzt%%DYBnLCIhaoG6$";
+        }
+
+        private string GenerateUniqueEmail()
+        {
+            return $"{Guid.NewGuid()}@berlinspaceflowers.com";
+        }
+    }
+}
